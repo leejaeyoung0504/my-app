502029362253b5cce1f3386c856a5c9f10df1313
leejaeyoung0504 <uruguayke34@gmail.com>
2025-02-17 17:34:39 +0900

    최초 생성

---
+++ app-pbdp/src/main/java/com/eds/pbdp/AppPbdpApplication.java
@@ -0,0 +1,13 @@
+package com.eds.pbdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPbdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPbdpApplication.class, args);
+	}
+
+}
---
+++ app-pbdp/src/test/java/com/eds/pbdp/AppPbdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eds.pbdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPbdpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
